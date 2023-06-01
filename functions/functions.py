26b54e1f09eda21748adac1160a53e18425e7d50
neurohunt <75321938+neurohunt@users.noreply.github.com>
2023-06-01 14:58:27 +0300

    Create functions.py

---
+++ functions/functions.py
@@ -0,0 +1,16 @@
+
+
+def return_first_five(liste):
+    liste.sort()
+    return liste[0]
+
+
+def return_unique_values(liste):
+    tuple(liste)
+
+#Vajag atgriezt sarakstu
+def multiply_by_5(liste):
+    for number in liste:
+        number*5
+        print(number)
+
